bfea2a73d09d5f9ba4683fa67640a449837deb6a
Gagik Simonyan <gagik.simonyan92@gmail.com>
2021-01-31 12:38:23 +0400

    Lesson 14 ... css-modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
   return ( 
-    <header className='header'>
+    <header className={s.header}>
       <img src="https://img.pngio.com/logo-business-png-free-logo-businesspng-transparent-images-business-logo-png-900_760.png" alt=""/>
     </header>
    );
