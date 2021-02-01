e49c0fbb44098569ad8316f0a92654e916bc9cdd
Gagik Simonyan <gagik.simonyan92@gmail.com>
2021-02-01 10:47:41 +0400

    Lesson 16 ... props introduction

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,8 +11,8 @@ const MyPosts = () => {
         <button>Add post</button>
       </div>
       <div className={s.posts}>
-        <Post/>
-        <Post/>
+        <Post message='Hi, how are you?'/>
+        <Post message='It is my first post'/>
       </div>
     </div>
    );
