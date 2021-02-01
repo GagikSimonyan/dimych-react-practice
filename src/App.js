708388dd015653a303f68877fae4ab7e6d0afdda
Gagik Simonyan <gagik.simonyan92@gmail.com>
2021-02-01 12:18:26 +0400

    Lesson 19 ... Learn Route and BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Dialogs from "./components/Dialogs/Dialogs.jsx";
 import Profile from "./components/Profile/Profile.jsx";
+import Dialogs from "./components/Dialogs/Dialogs.jsx";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header/>
-      <Navbar/>
-      <div className='app-wrapper-content'>
-        {/* <Profile/> */}
-        <Dialogs/>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header/>
+        <Navbar/>
+        <div className='app-wrapper-content'>
+          <Route path='/dialogs' component={Dialogs}/>
+          <Route path='/profile' component={Profile}/>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
