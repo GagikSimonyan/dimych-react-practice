42e03f2235ace49441319b9af4e8bf1e4552ddb3
Gagik Simonyan <gagik.simonyan92@gmail.com>
2021-02-01 11:47:02 +0400

    Lesson 18 ... create Dialogs page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Dialogs from "./components/Dialogs/Dialogs.jsx";
 import Profile from "./components/Profile/Profile.jsx";
 
 const App = () => {
@@ -9,7 +10,10 @@ const App = () => {
     <div className='app-wrapper'>
       <Header/>
       <Navbar/>
-      <Profile/>
+      <div className='app-wrapper-content'>
+        {/* <Profile/> */}
+        <Dialogs/>
+      </div>
     </div>
   );
 };
