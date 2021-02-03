b0a2804af1fc2ecd0b5278b15d7e811282cd6168
Gagik Simonyan <gagik.simonyan92@gmail.com>
2021-02-03 13:00:49 +0400

    Lesson 26 ... create DialogsItems and Message folders

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,24 +1,8 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
+import DialogItem from './DialogItem/DialogItem.jsx';
+import Message from './Message/Message.jsx';
 
-
-const DialogItem = (props) => {
-
-  let path = '/dialogs/' + props.id;
-
-  return ( 
-    <div className={s.dialog}>
-      <NavLink to={path} activeClassName={s.active}>{props.name}</NavLink>
-    </div>
-   );
-}
-
-const Message = (props) => {
-  return ( 
-    <div className={s.message}>{props.message}</div>
-   );
-}
  
 const Dialogs = (props) => {
 
