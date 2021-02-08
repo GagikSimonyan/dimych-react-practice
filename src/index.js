ee50ee2fc042a04381659e5bcb01646eeaf9bb34
Gagik Simonyan <gagik.simonyan92@gmail.com>
2021-02-08 07:19:55 +0400

    Lesson 35 ... delete render.js, subscribe, observer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from './render.js';
-import state from './redux/state';
+import App from './App';
+import state, { subscribe } from './redux/state';
+import { addPost, updateNewPostText } from './redux/state';
 
+
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,7 @@
-import { rerenderEntireTree } from './../render.js';
+// эта функция вызывается, когда меняется state
+let rerenderEntireTree = () => {
+  console.log('State is changed');
+}
 
 let state = {
   profilePage: {
@@ -31,16 +34,21 @@ let state = {
 
 window.state = state;
 
-export let addPost = () => {
+export const addPost = () => {
   let newPost = {id: 5, message: state.profilePage.newPostText, likesCount: 0};
   state.profilePage.posts.push(newPost);
   state.profilePage.newPostText = '';
   rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText;
   rerenderEntireTree(state);
+};
+
+export const subscribe = (observer) => {
+  // переопределяем rerenderEntireTree что наверху
+  rerenderEntireTree(observer);
 };
 
 export default state;
