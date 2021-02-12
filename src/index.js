3c0b1170c5de84e21cd7538bbbbfd277ed5798ef
Gagik Simonyan <gagik.simonyan92@gmail.com>
2021-02-12 15:58:18 +0400

    Lesson 47 ... copy state in ProfileReducer

--- src/index.js
+++ src/index.js
@@ -7,23 +7,14 @@ import store from './redux/redux-store.js';
 import { Provider } from 'react-redux';
 
 
-let rerenderEntireTree = (state) => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 
 // If you want to start measuring performance in your app, pass a function
