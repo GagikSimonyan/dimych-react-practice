2e60c0ec8bbd5dd3ff3c28684bdc71b88a5e44a2
Gagik Simonyan <gagik.simonyan92@gmail.com>
2021-02-25 11:00:27 +0400

    Lesson 62 ... follow-unfollow API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Users.module.css';
 import userPhoto from './../../assets/images/biggie.jpg';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 const Users = (props) => {
 
@@ -31,8 +32,32 @@ const Users = (props) => {
               </div>
               <div>
                 {u.followed
-                  ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                  : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                  ? <button onClick={() => {
+                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                      withCredentials: true,
+                      headers: {
+                        "API-KEY": "b1775b2f-c3a5-4509-8d9c-90b5629de7c3"
+                      }
+                    })
+                      .then(response => {
+                        if (response.data.resultCode === 0) {
+                          props.unfollow(u.id);
+                        }
+                      });
+                  }}>Unfollow</button>
+                  : <button onClick={() => {
+                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                      withCredentials: true,
+                      headers: {
+                        "API-KEY": "b1775b2f-c3a5-4509-8d9c-90b5629de7c3"
+                      }
+                    })
+                      .then(response => {
+                        if (response.data.resultCode === 0) {
+                          props.follow(u.id);
+                        }
+                      });
+                  }}>Follow</button>}
               </div>
             </span>
             <span>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -9,22 +9,32 @@ class UsersContainer extends React.Component {
 
   componentDidMount() {
     this.props.toggleIsFetching(true);
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-    .then(responce => {
-      this.props.toggleIsFetching(false);
-      this.props.setUsers(responce.data.items);
-      this.props.setTotalUsersCount(responce.data.totalCount);
-    });
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
+      withCredentials: true,
+      headers: {
+        "API-KEY": "b1775b2f-c3a5-4509-8d9c-90b5629de7c3"
+      }
+    })
+      .then(responce => {
+        this.props.toggleIsFetching(false);
+        this.props.setUsers(responce.data.items);
+        this.props.setTotalUsersCount(responce.data.totalCount);
+      });
   }
 
   onPageChanged = (pageNumber) => {
     this.props.setCurrentPage(pageNumber);
     this.props.toggleIsFetching(true);
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-    .then(responce => {
-      this.props.toggleIsFetching(false);
-      this.props.setUsers(responce.data.items);
-    });
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
+      withCredentials: true,
+      headers: {
+        "API-KEY": "b1775b2f-c3a5-4509-8d9c-90b5629de7c3"
+      }
+    })
+      .then(responce => {
+        this.props.toggleIsFetching(false);
+        this.props.setUsers(responce.data.items);
+      });
   }
 
   render() {
@@ -32,12 +42,12 @@ class UsersContainer extends React.Component {
       <>
         { this.props.isFetching ? <Preloader /> : null}
         <Users totalUsersCount={this.props.totalUsersCount}
-                pageSize={this.props.pageSize}
-                currentPage={this.props.currentPage}
-                onPageChanged={this.onPageChanged}
-                users={this.props.users}
-                follow={this.props.follow}
-                unfollow={this.props.unfollow}
+          pageSize={this.props.pageSize}
+          currentPage={this.props.currentPage}
+          onPageChanged={this.onPageChanged}
+          users={this.props.users}
+          follow={this.props.follow}
+          unfollow={this.props.unfollow}
         />
       </>
     );
