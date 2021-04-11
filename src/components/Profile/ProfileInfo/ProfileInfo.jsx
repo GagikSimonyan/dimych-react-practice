c83147e7b3b1f5b3785a5a74e4e07e4dc330aaa8
Gagik Simonyan <gagik.simonyan92@gmail.com>
2021-04-11 13:50:22 +0400

    Lesson 84 ... hooks, useState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
 import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 
 const ProfileInfo = (props) => {
@@ -19,7 +20,7 @@ const ProfileInfo = (props) => {
         <div><img src={props.profile.photos.large} alt="" /></div>
         <div>{props.profile.fullName}</div>
         <div>{props.profile.aboutMe}</div>
-        <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
       </div>
     </div>
   );
